e0d914cc772506fa30f772ebf52d71724051787d
kousik2000 <kousik.ramachandruni@gmail.com>
2023-02-19 08:24:35 +0000

    update2 notes pending

---
+++ src/components/RemainderItems/index.js
@@ -0,0 +1,41 @@
+import "./index.css";
+
+const RemainderItems = (props) => {
+  const { remainderDetails, onSetReminderClick, isDeleteButtonClicked } = props;
+  const {
+    id,
+    userRemainderText,
+    remainderDate,
+    remainderTime,
+  } = remainderDetails;
+
+  const onSetReminderButtonClick = () => {
+    onSetReminderClick(id);
+  };
+
+  const onDeleteButton = () => {
+    isDeleteButtonClicked(id);
+  };
+
+  return (
+    <li className="remainder-list">
+      <div className="remainder-item-container">
+        <div className="remainder-text-container">
+          <p className="remainder-text">{userRemainderText}</p>
+          <p className="remainder-text">{remainderDate}</p>
+          <p className="remainder-text">{remainderTime}</p>
+        </div>
+        <div className="remainder-button-container">
+          <button onClick={onSetReminderButtonClick} className="button-icons">
+            <i class="fa-solid fa-bell fa-lg"></i>
+          </button>
+          <button className="button-icons" onClick={onDeleteButton}>
+            <i className="fa-solid fa-trash-can fa-lg"></i>
+          </button>
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default RemainderItems;
--- src/components/StopWatch/index.js
+++ src/components/StopWatch/index.js
@@ -3,9 +3,96 @@ import "./index.css";
 import { Component } from "react";
 
 class StopWatch extends Component {
-  state = { stopWatch: [] };
+  state = { isTimeRunning: false, timeElapsedInSeconds: 0 };
+
+  updateTime = () => {
+    this.setState((prevState) => ({
+      timeElapsedInSeconds: prevState.timeElapsedInSeconds + 1,
+    }));
+  };
+
+  componentWillUnmount() {
+    clearInterval(this.timeInterval);
+  }
+
+  startTimer = () => {
+    this.timeInterval = setInterval(this.updateTime, 1000);
+    this.setState({ isTimeRunning: true });
+  };
+
+  pauseTimer = () => {
+    clearInterval(this.timeInterval);
+    this.setState({ isTimerRunning: false });
+  };
+
+  resetTimer = () => {
+    clearInterval(this.timeInterval);
+    this.setState({ isTimerRunning: false, timeElapsedInSeconds: 0 });
+  };
+
+  renderSeconds = () => {
+    const { timeElapsedInSeconds } = this.state;
+    const seconds = Math.floor(timeElapsedInSeconds % 60);
+
+    if (seconds < 10) {
+      return `0${seconds}`;
+    }
+    return seconds;
+  };
+
+  renderMinutes = () => {
+    const { timeElapsedInSeconds } = this.state;
+    const minutes = Math.floor(timeElapsedInSeconds / 60);
+
+    if (minutes < 10) {
+      return `0${minutes}`;
+    }
+    return minutes;
+  };
+
+  renderHours = () => {
+    const { timeElapsedInSeconds } = this.state;
+    const minutes = Math.floor(timeElapsedInSeconds / 3600);
+
+    if (minutes < 10) {
+      return `0${minutes}`;
+    }
+    return minutes;
+  };
+
+  renderDays = () => {
+    const { timeElapsedInSeconds } = this.state;
+    const minutes = Math.floor(timeElapsedInSeconds / 86400);
+
+    if (minutes < 10) {
+      return `0${minutes}`;
+    }
+    return minutes;
+  };
+
   render() {
-    return <h1>Your are in page Stopwatch</h1>;
+    const time = `${this.renderDays()}:${this.renderHours()}:${this.renderMinutes()}:${this.renderSeconds()}`;
+    return (
+      <div className="bg-container">
+        <h1 className="main-head">Stop Watch</h1>
+        <div className="card-container">
+          <div className="display-container">
+            <h1 className="timer">{time}</h1>
+          </div>
+          <div className="button-container">
+            <button className="button" onClick={this.startTimer}>
+              Start
+            </button>
+            <button className="button" onClick={this.pauseTimer}>
+              Pause
+            </button>
+            <button className="button" onClick={this.resetTimer}>
+              Reset
+            </button>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
